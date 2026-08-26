bbf3914b021be79bd5b7f59b95b9bac04726502d
harishkg4 <83876706+harishkg4@users.noreply.github.com>
2026-08-26 12:30:42 -0700

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict: [Int: Int] = [:]
+        
+        for (index, value) in nums.enumerated() {
+            let difference = target - nums[index]
+            if dict[difference] != nil {
+                if let firstIndex = dict[difference] {
+                    return [firstIndex, index]
+                }
+            } else {
+                
+                dict[value] = index 
+            }
+            
+        }
+        return []
+    }
+}
